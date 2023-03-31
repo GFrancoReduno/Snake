5abed42cf478abadbe753e06ac69783dd93fb557
GFrancoReduno <francoreduno@outlook.com>
2023-03-31 13:27:30 -0300

    del 0 al 1

---
+++ 00_python.py
@@ -0,0 +1,27 @@
+#hello world
+
+print ("perro salchicha")
+print ('perro salchicha')
+
+'''
+comentario
+entre
+lineas
+'''
+
+"""
+otro
+comentario
+entre
+lineas
+"""
+
+#consultar el tipo de dato
+print (type("perro salchicha")) #tipo 'string'
+print (type(3)) #tipo 'int'
+print (type(1.3)) #tipo 'float'
+print (type(1+3j)) #tipo 'complex'
+print (type(True)) #tipo 'bool'
+
+print(type(print("es una funcion"))) #tipo 'NoneType'
+
